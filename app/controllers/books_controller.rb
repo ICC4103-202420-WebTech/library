7d08d3803c76593bcb5825e055ed4ddbbca2e953
Andrés Howard <ahowardm@gmail.com>
2024-09-16 10:25:11 -0300

    clase update de books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,31 +1,47 @@
 class BooksController < ApplicationController
+  before_action :get_book_by_id, only: [:show, :edit, :update]
+  before_action :get_all_authors, only: [:new, :edit]
   
   # def index ; end
   def index
     @books = Book.all
   end
 
-  def show
-    @book = Book.find params['id']
-  end
+  def show ; end
 
   def new
     @book = Book.new
-    @authors = Author.all
   end
 
   def create
     book = Book.new book_params
     if book.save
       redirect_to book_path(book)
-      # redirect_to book
     else
       redirect_to new_book_path
+    end
+  end
+
+  def edit ; end
+
+  def update
+    if @book.update book_params
+      redirect_to book_path(@book)
+    else
+      redirect_to edit_book_path(@book), alert: @book.errors.full_messages
     end
   end
 
   private
     def book_params
       params.require(:book).permit(:title, :year_of_publication, :author_id)
+    end
+
+    def get_book_by_id
+      @book = Book.find params['id']
+    end
+
+    def get_all_authors
+      @authors = Author.all
     end
 end
